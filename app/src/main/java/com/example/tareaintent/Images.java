1659d37c061bfd26504e631927c7b26a6c5a7db5
Samir <samilr.sg2@gmail.com>
2022-10-14 17:06:17 -0400

    -Add Share imagen

--- app/src/main/java/com/example/tareaintent/Images.java
+++ app/src/main/java/com/example/tareaintent/Images.java
@@ -2,7 +2,11 @@ package com.example.tareaintent;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -11,12 +15,17 @@ import android.widget.TextView;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.ByteArrayOutputStream;
 import java.util.Random;
 
 public class Images extends AppCompatActivity {
     Button btnGoBack, btnExitApp, btnShowReflexion, btnShowPersonalSuperation;
     TextView tvName;
     ImageView imgV;
+    String gender;
+    int[] menPhrases, womenPhrases, menRefletion, womenRefletion;
+    int imgSelected;
+    Random rand = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +36,12 @@ public class Images extends AppCompatActivity {
         Bundle getName = getIntent().getExtras();
         tvName.setText("Hola, "+ getName.getString("nombre"));
 
+        Bundle getGender = getIntent().getExtras();
+        gender = getGender.getString("gender");
+        menPhrases = new int[]{R.drawable.sh1, R.drawable.sh2, R.drawable.sh3, R.drawable.sh4, R.drawable.sh5, R.drawable.sh6, R.drawable.sh7, R.drawable.sh8, R.drawable.sh9, R.drawable.sh10};
+        womenPhrases = new int[]{R.drawable.s1, R.drawable.s2, R.drawable.s3, R.drawable.s4, R.drawable.s5, R.drawable.s6, R.drawable.s7, R.drawable.s8, R.drawable.s9, R.drawable.s10};
+        menRefletion = new int[]{R.drawable.rh1, R.drawable.rh2, R.drawable.rh3, R.drawable.rh4,R.drawable.rh5,R.drawable.rh6, R.drawable.rh7,R.drawable.rh8,R.drawable.rh9, R.drawable.rh10};
+        womenRefletion = new int[]{R.drawable.r1, R.drawable.r2, R.drawable.r3, R.drawable.r11,R.drawable.r5,R.drawable.r6, R.drawable.r7,R.drawable.r8,R.drawable.r9, R.drawable.r10};
     }
 
     public void component(){
@@ -37,11 +52,23 @@ public class Images extends AppCompatActivity {
         btnExitApp = findViewById(R.id.btnExitAppFromImg);
         imgV = findViewById(R.id.imgIMG);
     }
+    public void shareImages(View view) {
+        Bitmap b = BitmapFactory.decodeResource(getResources(),imgSelected);
+        Intent share = new Intent(Intent.ACTION_SEND);
+        share.setType("image/jpeg");
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        b.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(getContentResolver(), b, "Title", null);
+        Uri imageUri =  Uri.parse(path);
+        share.putExtra(Intent.EXTRA_STREAM, imageUri);
+        startActivity(Intent.createChooser(share, "Select"));
+    }
 
     public void goToInfoActivityFromImg(View view) {
         Intent intent = new Intent(this, UserInfo.class);
         startActivity(intent);
     }
+
     public void ExitAppFromUserImg(View view) {        final  AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setMessage("Estas seguro que quieres salir?")
                 .setPositiveButton("ok", new DialogInterface.OnClickListener() {
@@ -59,18 +86,14 @@ public class Images extends AppCompatActivity {
     }
 
     public void ShowPersonalPhrases(View view){
-        Bundle getGender = getIntent().getExtras();
-        String gender = getGender.getString("gender");
-        int[] menPhrases = {R.drawable.sh1, R.drawable.sh2, R.drawable.sh3, R.drawable.sh4,R.drawable.sh5,R.drawable.sh6, R.drawable.sh7,R.drawable.sh8,R.drawable.sh9, R.drawable.sh10};
-        int[] womenPhrases = {R.drawable.s1, R.drawable.s2, R.drawable.s3, R.drawable.s4,R.drawable.s5,R.drawable.s6, R.drawable.s7,R.drawable.s8,R.drawable.s9, R.drawable.s10};
-        Random rand = new Random();
-
         switch (gender) {
             case "Hombre":
-                imgV.setImageResource(menPhrases[rand.nextInt(menPhrases.length)]);
+                imgSelected = menPhrases[rand.nextInt(menPhrases.length)];
+                imgV.setImageResource(imgSelected);
                 break;
             case "Mujer":
-                imgV.setImageResource(womenPhrases[rand.nextInt(womenPhrases.length)]);
+                imgSelected = womenPhrases[rand.nextInt(womenPhrases.length)];
+                imgV.setImageResource(imgSelected);
                 break;
             default:
                 imgV.setImageResource(R.drawable.sh1);
@@ -78,18 +101,14 @@ public class Images extends AppCompatActivity {
     }
 
     public void ShowLifeRefletion(View view){
-        Bundle getGender = getIntent().getExtras();
-        String gender = getGender.getString("gender");
-        int[] menRefletion = {R.drawable.rh1, R.drawable.rh2, R.drawable.rh3, R.drawable.rh4,R.drawable.rh5,R.drawable.rh6, R.drawable.rh7,R.drawable.rh8,R.drawable.rh9, R.drawable.rh10};
-        int[] womenRefletion = {R.drawable.r1, R.drawable.r2, R.drawable.r3, R.drawable.r11,R.drawable.r5,R.drawable.r6, R.drawable.r7,R.drawable.r8,R.drawable.r9, R.drawable.r10};
-        Random rand = new Random();
-
         switch (gender) {
             case "Hombre":
-                imgV.setImageResource(menRefletion[rand.nextInt(menRefletion.length)]);
+                imgSelected = menRefletion[rand.nextInt(menRefletion.length)];
+                imgV.setImageResource(imgSelected);
                 break;
             case "Mujer":
-                imgV.setImageResource(womenRefletion[rand.nextInt(womenRefletion.length)]);
+                imgSelected = womenRefletion[rand.nextInt(womenRefletion.length)];
+                imgV.setImageResource(imgSelected);
                 break;
             default:
                 imgV.setImageResource(R.drawable.sh1);
